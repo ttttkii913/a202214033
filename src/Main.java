c7cb656cf47b11513a6e7f93a81d5d815b009ef4
ttttkii913 <dbgkdud821@daum.net>
2024-03-27 12:38:07 +0900

    sixth

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,8 @@ public class Main {
 
 	public static void main(String[] args) {
 		System.out.println("유하영");
-		System.err.println("하나");
+		System.out.println("하나");
+		System.out.println("둘");
 	}
 
 }
